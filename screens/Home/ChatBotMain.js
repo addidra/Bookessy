ab73540c8d004332ff48862e4045a7134ad67a45
addidra <mohammadaqibaha786@gmail.com>
2024-02-20 13:51:47 +0530

    initializing chatbot

--- screens/Home/ChatBotMain.js
+++ screens/Home/ChatBotMain.js
@@ -1,10 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import { dummyMessages } from "./constant";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../../colors";
 
 const ChatBotMain = () => {
+  const [messages, setMessages] = useState(dummyMessages);
   return (
     <View style={styles.container}>
-      <Text style={{ color: "pink" }}>ChatBotMain</Text>
+      <MaterialCommunityIcons
+        name="robot-excited"
+        size={64}
+        color={colors.secondary}
+        style={{ alignSelf: "center" }}
+      />
+      {messages.length > 0 ? (
+        <View>
+          <Text style={{ fontSize: 35, color: colors.secondary }}>
+            Assistant
+          </Text>
+          <View style={styles.chatContainer}>
+            <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+              {messages.map((msg, index) => {
+                if (msg.role == "assistant") {
+                  if (msg.content.includes("https")) {
+                    // an ai img
+                  } else {
+                    // text response
+                  }
+                } else {
+                  //user input
+                }
+              })}
+            </ScrollView>
+          </View>
+        </View>
+      ) : (
+        <></>
+      )}
+      {/* <FontAwesome name="microphone" size={44} color="pink" /> */}
     </View>
   );
 };
@@ -15,7 +50,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#242038",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "space-between",
+    padding: 20,
+  },
+  chatContainer: {
+    backgroundColor: "gray",
+    opacity: 0.4,
+    height: "75%",
+    borderRadius: 20,
+    padding: 10,
   },
 });
---
+++ screens/Home/constant.js
@@ -0,0 +1,10 @@
+export const dummyMessages = [
+  { role: "user", content: "How are you?" },
+  { role: "assistant", content: "I'm fine, How may i help you today." },
+  { role: "user", content: "create an image of man with fire sword" },
+  {
+    role: "assistant",
+    content:
+      "https://c4.wallpaperflare.com/wallpaper/790/92/892/weapons-fire-the-game-sword-wallpaper-preview.jpg",
+  },
+];
